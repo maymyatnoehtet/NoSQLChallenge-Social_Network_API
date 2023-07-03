dcbc99a9187d21fd79a212336b05e828cfa55d2f
maymyatnoehtet <may.mn.htet@gmail.com>
2023-07-03 19:45:08 +1000

    adding userController and userRoutes file. get all users and user by id

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+const { User } = require("../models");
+
+const userController = {
+  getAllUsers(req, res) {
+    User.find({})
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+};
+
+module.exports = userController;
