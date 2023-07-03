8dc8bbb9564dd88d562b318b4b8605ba8484937a
maymyatnoehtet <may.mn.htet@gmail.com>
2023-07-03 18:32:25 +1000

    add reaction and thought data models

---
+++ models/thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thoughts = model("Thoughts", thoughtSchema);
+
+module.exports = Thoughts;
